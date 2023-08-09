aa949734d757dbb6a8f55dbdb723b366dd50a309
Vinaykumar8919 <124648756+Vinaykumar8919@users.noreply.github.com>
2023-08-09 23:09:35 +0530

    Update longestConsecutiveSequenc

--- longestConsecutiveSequence.py
+++ longestConsecutiveSequence.py
@@ -17,20 +17,41 @@
 
 
 
+# class Solution:
+#     def longestConsecutive(self, nums: List[int]) -> int:
+#         if not nums:
+#             return 0
+#         nums = set(nums)
+#         nums = sorted(nums) 
+#         maxcount = 1
+#         count = 1
+#         for i in range(len(nums) - 1):
+#             if nums[i] + 1 == nums[i + 1]:
+#                 count += 1
+#             else:
+#                 maxcount = max(maxcount, count)
+#                 count = 1
+#         maxcount = max(maxcount, count)
+#         return maxcount
+
+
+
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        nums = set(nums)
-        nums = sorted(nums) 
-        maxcount = 1
-        count = 1
-        for i in range(len(nums) - 1):
-            if nums[i] + 1 == nums[i + 1]:
-                count += 1
-            else:
-                maxcount = max(maxcount, count)
-                count = 1
-        maxcount = max(maxcount, count)
-        return maxcount
+        nums_set = set(nums)
+        ans = 0
+        for i in range(len(nums)):
+            current_element = nums[i]
+            count = 0
+            previous_element = current_element-1
+            if previous_element not in nums_set: # finding smallest  element in the chain
+                while current_element in nums_set:
+                    current_element+=1
+                    count+=1
+            ans = max(ans,count)
+        return ans
 
+
+
+
+        
