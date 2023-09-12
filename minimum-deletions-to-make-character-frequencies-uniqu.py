70c33ab8fb15ee62fbe7279e50cebd4dd9561053
Vinaykumar8919 <124648756+Vinaykumar8919@users.noreply.github.com>
2023-09-12 21:36:56 +0530

    Create minimum-deletions-to-make-character-frequencies-uniqe

---
+++ minimum-deletions-to-make-character-frequencies-uniqu.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minDeletions(self, s: str) -> int:
+        letter_count = {}
+        for i in s:
+            if i in letter_count:
+                letter_count[i]+=1
+            else:
+                letter_count[i]=1
+        temp = []
+        for i in letter_count.values():
+            temp.append(i)
+        res = 0
+        print(temp)
+        for index, ele in enumerate(temp):
+            while temp[index] in set(temp[:index]+temp[index+1:]):
+                temp[index]-=1 
+                if temp[index]==0:
+                    del temp[index]
+                res +=1 
+        return res
