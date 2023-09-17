92a042e303220adb6e5247d8711476c8e7e450bf
Vinaykumar8919 <124648756+Vinaykumar8919@users.noreply.github.com>
2023-09-17 19:28:54 +0530

    Update check-if-number-is-a-sum-of-powers-of-three.py

--- check-if-number-is-a-sum-of-powers-of-three.py
+++ check-if-number-is-a-sum-of-powers-of-three.py
@@ -1,16 +1,16 @@
-class Solution:
-    def checkPowersOfThree(self, n: int) -> bool:
-        powers3 = [3 ** i for i in range(20)]
-        listLessThanN = [x for x in powers3 if x<=n]
-        print(listLessThanN)
-        while listLessThanN:
-            n = n - listLessThanN[-1]
-            listLessThanN.pop()
-            listLessThanN = [x for x in listLessThanN if x<=n]
-            print(listLessThanN, n)
-        if n==0:
-            return True
-        return False
+# class Solution:
+#     def checkPowersOfThree(self, n: int) -> bool:
+#         powers3 = [3 ** i for i in range(20)]
+#         listLessThanN = [x for x in powers3 if x<=n]
+#         print(listLessThanN)
+#         while listLessThanN:
+#             n = n - listLessThanN[-1]
+#             listLessThanN.pop()
+#             listLessThanN = [x for x in listLessThanN if x<=n]
+#             print(listLessThanN, n)
+#         if n==0:
+#             return True
+#         return False
         
             
 
